d4633164e6ab37a9546933a141c669486db1c369
Adnan <adbak.dev@gmail.com>
2024-10-27 16:18:44 +0100

    search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import SearchForm from "@/components/SearchForm";
 
-function Home() {
-	return <div className="text-2xl">Home</div>;
+function Home({ searchParams }: { searchParams: { query?: string } }) {
+	return (
+		<>
+			<section className="pink_container">
+				<h1 className="heading">
+					Pitch your startup Submit your startup to be featured on Directory
+				</h1>
+				<p className="sub-heading !max-w-3xl">
+					Submit Ideas, Vote on Pitches, and Discover the best ideas
+				</p>
+				<SearchForm query={searchParams.query} />
+			</section>
+		</>
+	);
 }
 
 export default Home;
